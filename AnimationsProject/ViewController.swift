d337588441089531a74870cdcc49ebe6a8bc71f5
Nandu <nandu@Nandus-MacBook-Pro.local>
2017-10-17 21:12:34 -0700

    learned about different kind of properties

--- AnimationsProject/ViewController.swift
+++ AnimationsProject/ViewController.swift
@@ -10,17 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var objectAnim: UIButton!
-    var iImageView:UIImageView {
+    var iImageView:UIImageView? = nil;
+    
+    //Calculated properties dont modify the same variable to goes into stackoverflow exception
+    //calculated properties are used only to do some calculations and modyfy the values of other properties
+    //in otherwords calculated properties depends on other properties to calculate values.
+    //Below commented code is wrong usage of calculated properties.
+    /*
+    var name:String {
         get {
-            return self.iImageView;
+            return name;
         }
-        set {
-            self.iImageView = iImageView;
+    
+        set(newName){
+            name = newName;
         }
     }
+     */
+    
+    //Not an optional property
+    //var name:String = String()
+    
+    //Optional property, values can be retrived by ! operator ex: self.variablename!
+    var name:String? = nil;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let latinCapitalLetterA: Character = "\u{41}"
+        
+        let cyrillicCapitalLetterA: Character = "\u{0410}"
+        
+        if latinCapitalLetterA != cyrillicCapitalLetterA {
+            print("These two characters are not equivalent.\(latinCapitalLetterA)------\(cyrillicCapitalLetterA)")
+        }
+        self.name = "nandu";
+        print("My name is "+self.name!)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -34,12 +59,17 @@ class ViewController: UIViewController {
     @IBAction func animateObject(_ sender: Any) {
         let _image = UIImage.init(imageLiteralResourceName: "sample.jpg");
         self.iImageView = UIImageView.init(frame: CGRect(x:30, y:30, width:100, height:100));
-        self.iImageView.image = _image;
-        self.view.addSubview(self.iImageView);
+        self.iImageView?.image = _image;
+        self.view.addSubview(self.iImageView!);
     }
     
     @IBAction func animateImageView(_ sender: Any) {
-        
+        UIView.animate(withDuration: 0.5) {
+            var cgPoint = self.iImageView?.center;
+            cgPoint!.x += 100
+            cgPoint!.y += 100
+            self.iImageView?.center = cgPoint!;
+        }
     }
 }
 
